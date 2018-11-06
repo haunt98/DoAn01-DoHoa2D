9a5e9a635a8542ef2e49e4928e355210e63661b1
Tran Hau <ngtranhau@gmail.com>
2018-11-06 13:47:33 +0700

    Tao solution

---
+++ Programs/1612180_1612677/1612180_1612677/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1612180_1612677
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
